d2129e177aa096d24d668c11af20bf6e5022447a
paulinaika <paulina.ika@gmail.com>
2017-05-17 13:24:04 +1000

    test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+
+// yarn add enzyme --dev
+// yarn add react-test-renderer --dev
+
+
+it('handleRestart() reset quiz state', () => {
+  let component = shallow(<App />);
+  component.instance().setState = jest.fn();
+
+  component.instance().state = {
+    progress: 2,
+    selected: 'Meow',
+    score: 4
+  };
+
+  component.instance().handleRestart();
+
+  expect(component.instance().setState).toHaveBeenCalledWith({
+    progress: 0,
+    selected: 'None Yet',
+    score: 0
+  });
 });
--- src/ProgressBar.js
+++ src/ProgressBar.js
@@ -1,9 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
+// import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-class ProgressBar extends Component {
-  render() {
-    return <p>{this.props.current_step}/{this.props.question_length}</p>
-  }
+//stateless Component
+
+const ProgressBar = ({current_step, question_length}) => {
+  return <p>{current_step}/{question_length}</p>
 }
+
+ProgressBar.propTypes = {
+  current_step: PropTypes.number.isRequired,
+  question_length: PropTypes.number.isRequired,
+}
+
+// class ProgressBar extends Component {
+//   render() {
+//     return <p>{this.props.current_step}/{this.props.question_length}</p>
+//   }
+// }
 
 export default ProgressBar;
